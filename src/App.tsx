12d8c045456de80bc2f00a66c31c320a88abc957
[dyad] <nuramirah@ubbim.com>
2026-03-09 15:08:48 +0800

    Reverted all changes back to version f9b2ad3638b4cd23c89e88e787965bcfdabce6e7

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Services from "./pages/Services";
-import Projects from "./pages/Projects";
-import SustainabilityReport from "./pages/SustainabilityReport";
-import Awards from "./pages/Awards";
-import Whistleblower from "./pages/Whistleblower";
-import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -22,22 +14,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/sustainability-report" element={<SustainabilityReport />} />
-              <Route path="/awards" element={<Awards />} />
-              <Route path="/whistleblower" element={<Whistleblower />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
