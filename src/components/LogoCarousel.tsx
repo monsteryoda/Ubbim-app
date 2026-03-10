92bb744912447b4ed6519357f7cbc7c825410c56
[dyad] <nuramirah@ubbim.com>
2026-03-10 15:28:37 +0800

    Reverted all changes back to version 71769c86ec71c415f88b73ab13a16c4147175d72

--- src/components/LogoCarousel.tsx
+++ src/components/LogoCarousel.tsx
@@ -42,7 +42,13 @@ const LogoCarousel: React.FC<LogoCarouselProps> = ({ logos }) => {
         >
           {logos.map((logo, index) => (
             <div key={index} className="w-full flex-shrink-0 px-4">
-              {/* Logo image removed */}
+              <div className="flex items-center justify-center">
+                <img
+                  src={logo.image}
+                  alt={logo.name}
+                  className="h-16 md:h-20 object-contain bg-transparent"
+                />
+              </div>
             </div>
           ))}
         </div>
