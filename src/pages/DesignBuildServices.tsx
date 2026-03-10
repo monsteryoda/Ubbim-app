d8ca849a518a8d24edf23a50f32e5689d7a6aac8
[dyad] <nuramirah@ubbim.com>
2026-03-10 12:43:14 +0800

    [dyad] Fixed chooseus.jpg image path - wrote 2 file(s)

--- src/pages/DesignBuildServices.tsx
+++ src/pages/DesignBuildServices.tsx
@@ -212,7 +212,7 @@ const DesignBuildServices = () => {
             <div className="relative">
               <div className="aspect-[16/10] overflow-hidden rounded-2xl shadow-2xl">
                 <img 
-                  src="/src/assets/chooseus.jpg" 
+                  src="/chooseus.jpg" 
                   alt="Construction Site" 
                   className="w-full h-full object-cover"
                 />
