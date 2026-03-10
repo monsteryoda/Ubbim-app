995e8efc049b5b0533aa85cd78ce15bff00906a6
[dyad] <nuramirah@ubbim.com>
2026-03-10 09:35:07 +0800

    [dyad] Removed ISO 14001 certification from grid - wrote 1 file(s)

--- src/components/CertificationsGrid.tsx
+++ src/components/CertificationsGrid.tsx
@@ -21,16 +21,7 @@ const CertificationsGrid: React.FC<CertificationsGridProps> = ({ certifications 
   const [selectedCertification, setSelectedCertification] = useState<Certification | null>(null);
 
   // Sample certifications data
-  const defaultCertifications: Certification[] = [
-    {
-      id: "1",
-      title: "ISO 14001:2015",
-      organization: "ACM-CCAS Limited",
-      year: "2019",
-      image: "/src/assets/certifications/iso-14001.png",
-      description: "Environmental Management System Certification"
-    }
-  ];
+  const defaultCertifications: Certification[] = [];
 
   const displayCertifications = certifications || defaultCertifications;
 
