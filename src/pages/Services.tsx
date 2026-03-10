830c2bad142cf67e2ac4e738eee572209f3520c5
[dyad] <nuramirah@ubbim.com>
2026-03-10 11:34:28 +0800

    [dyad] Fixed logo image path to public folder - wrote 4 file(s)

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -48,7 +48,7 @@ const Services = () => {
           <div className="flex items-center justify-between h-16">
             <Link to="/" className="flex items-center space-x-2">
               <img 
-                src="/src/assets/ubbim-logo.png" 
+                src="/ubbim-logo.png" 
                 alt="UBBIM Logo" 
                 className="h-8 w-auto"
               />
