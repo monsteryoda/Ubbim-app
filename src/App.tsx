743706826e195526e485fb9a894d2712a4009215
[dyad] <nuramirah@ubbim.com>
2026-03-12 12:54:07 +0800

    Reverted all changes back to version 3bb5b416b001161eb22a998985916f586a6abc6e

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,28 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import DigitalSolutions from "./pages/DigitalSolutions";
 import Award from "./pages/Award";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/digital-solutions" element={<DigitalSolutions />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/award" element={<Award />} />
-      </Routes>
+      <div className="min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/award" element={<Award />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
