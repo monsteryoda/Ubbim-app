254282e6353472fd498305e8a4510efb4bd52ca9
[dyad] <nuramirah@ubbim.com>
2026-03-13 13:02:33 +0800

    [dyad] Added page title for Aerial Analytics Solutions - wrote 1 file(s)

--- src/pages/AerialAnalytics.tsx
+++ src/pages/AerialAnalytics.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ArrowLeft, MapPin, Phone, Mail, Plane, Camera, Map, FileText, Shield, TrendingUp, CheckCircle, Building2, Clock, Leaf, Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -8,6 +8,13 @@ import { Card, CardContent } from "@/components/ui/card";
 import Navbar from "@/components/Navbar";
 
 const AerialAnalytics = () => {
+  useEffect(() => {
+    document.title = "Aerial Analytics Solutions - UBBIM";
+    return () => {
+      document.title = "UBBIM - Integrated Solutions for the Built Environment";
+    };
+  }, []);
+
   const services = [
     {
       title: "Land Surveying",
