192f3c7e58e849f3d2adab0c5ae4f0dcc60b399d
[dyad] <nuramirah@ubbim.com>
2026-03-27 16:41:12 +0800

    [dyad] wrote 2 file(s)

--- src/pages/DesignBuildServices.tsx
+++ src/pages/DesignBuildServices.tsx
@@ -90,7 +90,7 @@ const DesignBuildServices = () => {
     {
       title: "General Contracting",
       description: "Rely on our skilled team to handle every aspect of construction, from sourcing materials to managing subcontractors.",
-      image: "/chooseus.jpg",
+      image: "/general-contracting.jpg",
       fallbackImage: "https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=800&q=80"
     }
   ];
